44b1a859ee32e0bb6fb6b6e70953c5b77078a67a
jwjin <jwjin@me.com>
2016-06-23 15:49:00 +0900

    tt

---
+++ test_process.js
@@ -0,0 +1,14 @@
+cl = console.log;
+
+//cl(process.env);
+//cl(process.version);
+//cl(process.versions);
+//cl(process.arch);
+//cl(process.argv);
+//process.exit();
+//console.log('hello world')
+//cl(process.memoryUsage());
+//for(var i = 1; i < 10000; i++) {
+//  cl('hello');
+//}
+//cl(process.uptime());
